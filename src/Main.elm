f8a321fee373d9f1d9731cc506facd8ff4304396
Kent Kawanaka <k-kawanaka@plott.co.jp>
2018-05-29 14:45:15 +0900

    再生中もBPMを変更可能に また、BPMの直接入力を禁止

--- src/Main.elm
+++ src/Main.elm
@@ -7,33 +7,50 @@ import Time
 import Result
 import Task
 import String
-import Debug
+import Delay
 
 
 type alias Model =
-    { isRunning : Bool, bpm : Int, tapList : List Float }
+    { isRunning : Bool, bpm : Int, tapList : List Float, holdDirection : Maybe BpmDirection, holdBeginTime : Maybe Float }
+
+
+type BpmDirection
+    = Up
+    | Down
 
 
 type Msg
-    = InputBpm String
-    | Toggle
+    = Toggle
     | Tap
     | TapTime Float
+    | BpmChange BpmDirection
+    | BpmHold BpmDirection
+    | BpmHoldStart BpmDirection
+    | BpmRelease
+    | GotHoldBeginTime Float
+    | GotHoldJudgeTime BpmDirection Float
+
+
+maxBpm : Int
+maxBpm =
+    500
+
+
+defaultBpm : Int
+defaultBpm =
+    120
 
 
 initialModel : Model
 initialModel =
-    { isRunning = False, bpm = 120, tapList = [] }
+    { isRunning = False, bpm = 120, tapList = [], holdDirection = Nothing, holdBeginTime = Nothing }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        InputBpm s ->
-            String.toInt s |> Result.withDefault 120 |> \bpm -> { model | bpm = bpm } ! []
-
         Toggle ->
-            {model| isRunning = not model.isRunning} ! [ bpmToInterval model.bpm |> toggle ]
+            { model | isRunning = not model.isRunning } ! [ bpmToInterval model.bpm |> toggle ]
 
         Tap ->
             model ! [ Time.now |> Task.perform TapTime ]
@@ -41,12 +58,52 @@ update msg model =
         TapTime t ->
             let
                 tapList =
-                    t :: model.tapList |> Debug.log "taplist"
+                    t :: model.tapList
 
                 ( newBpm, newList ) =
                     newBpmAndTapList tapList
+
+                bpm =
+                    Maybe.withDefault model.bpm newBpm |> min maxBpm
             in
-                { model | bpm = Maybe.withDefault model.bpm newBpm, tapList = newList } ! []
+                { model | bpm = bpm, tapList = newList } ! [ bpmToInterval bpm |> newInterval ]
+
+        BpmChange d ->
+            let
+                newBpm =
+                    case d of
+                        Up ->
+                            model.bpm + 1 |> min maxBpm
+
+                        Down ->
+                            model.bpm - 1 |> max 0
+            in
+                { model | bpm = newBpm } ! [ bpmToInterval newBpm |> newInterval ]
+
+        BpmHold d ->
+            model
+                ! [ Delay.after 1 Time.second <| BpmHoldStart d
+                  , Time.now |> Task.perform GotHoldBeginTime
+                  ]
+
+        BpmHoldStart d ->
+            model ! [ Time.now |> Task.perform (GotHoldJudgeTime d) ]
+
+        BpmRelease ->
+            { model | holdDirection = Nothing, holdBeginTime = Nothing } ! []
+
+        GotHoldBeginTime f ->
+            { model | holdBeginTime = Just f } ! []
+
+        GotHoldJudgeTime d f ->
+            { model
+                | holdDirection =
+                    if (Maybe.map ((-) f >> flip (>) 900) model.holdBeginTime |> Maybe.withDefault False) then
+                        Just d
+                    else
+                        Nothing
+            }
+                ! []
 
 
 main : Program Never Model Msg
@@ -55,22 +112,47 @@ main =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+subscriptions m =
+    case m.holdDirection of
+        Just d ->
+            Time.every (20 * Time.millisecond) (always (BpmChange d))
+
+        Nothing ->
+            Sub.none
 
 
 view : Model -> Html.Html Msg
 view model =
     Html.ul []
         [ Html.li []
-            [ Html.input [ Html.Events.onInput InputBpm, Html.Attributes.value <| toString model.bpm ] []
-            ]
+            [ Html.text <| "BPM: " ++ (toString model.bpm) ]
         , Html.li []
-            [ Html.button [ Html.Events.onClick Toggle ] [ Html.text <| if model.isRunning then "stop" else "start" ]
-             
+            [ Html.button [ Html.Events.onClick Toggle ]
+                [ Html.text <|
+                    if model.isRunning then
+                        "stop"
+                    else
+                        "start"
+                ]
             ]
         , Html.li []
             [ Html.button [ Html.Events.onClick Tap ] [ Html.text "tap" ]
+            ]
+        , Html.li []
+            [ Html.button
+                [ Html.Events.onClick <| BpmChange Up
+                , Html.Events.onMouseDown (BpmHold Up)
+                , Html.Events.onMouseUp BpmRelease
+                ]
+                [ Html.text "up" ]
+            ]
+        , Html.li []
+            [ Html.button
+                [ Html.Events.onClick <| BpmChange Down
+                , Html.Events.onMouseDown (BpmHold Down)
+                , Html.Events.onMouseUp BpmRelease
+                ]
+                [ Html.text "down" ]
             ]
         ]
 
@@ -111,7 +193,7 @@ newBpmAndTapList tapList =
     else
         let
             diffList =
-                createDiffList (-) tapList |> List.take 3 |> Debug.log "diff"
+                createDiffList (-) tapList |> List.take 3
         in
             if List.all (\x -> x < 6000) diffList then
                 ( List.sum diffList |> Time.inSeconds |> (flip (/) 3) |> intervalToBpm |> Just, List.take 4 tapList )
@@ -120,3 +202,6 @@ newBpmAndTapList tapList =
 
 
 port toggle : Float -> Cmd msg
+
+
+port newInterval : Float -> Cmd msg
